441b28b0e01832760dde20ca8f65dd774ac06a42
genc4ik <50083271+genc4ik@users.noreply.github.com>
2025-12-04 13:01:31 +0300

    Update external_pwm.c

--- src/main/rx/external_pwm.c
+++ src/main/rx/external_pwm.c
@@ -9,7 +9,7 @@ static volatile uint16_t extPwmUs = 0;
 
 void externalPwmInit(void)
 {
-    IO_t io = IOGetByTag(IO_TAG(PB14));
+    IO_t io = IOGetByTag(IO_TAG(PB10));
     IOInit(io, OWNER_RX, RESOURCE_INPUT, 0);
     IOConfigGPIO(io, IOCFG_IPD); // Input Pull-Down
 }
@@ -29,7 +29,7 @@ void externalPwmUpdate(void)
     }
     lastReadTime = now;
     
-    IO_t io = IOGetByTag(IO_TAG(PB14));
+    IO_t io = IOGetByTag(IO_TAG(PB10));
     bool currentState = IORead(io);
     
     // Обнаружение фронтов
